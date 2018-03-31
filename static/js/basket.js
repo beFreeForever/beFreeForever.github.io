f4c22e2460af53ce8b404267f36099b7b8fee7c4
Артем Крюченков <a.kryuchenkov2@gmail.com>
2018-03-31 12:58:51 +0300

    fix

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,15 +1,9 @@
 function initBacket() {
     let count = getCount();
-    console.log($('#basket'))
     if (count == 0) {
-        // $('#counter'.)
-        console.log('asd')
-        // console.log($('#basket')[0].setAttribute('display', 'none'));
-        // console.log($('#basket')[0].setAttribute('display', 'none'));
         init();
         $('#basket').hide();
     } else {
-        // $('#basket')[0].css('visibility', 'visible');
         $('#basket').show();
         $('#counter')[0].textContent = count;
     }
@@ -28,21 +22,20 @@ function addInBacket(name) {
     Materialize.toast('Продукт добавлен в корзину', 1500, 'rounded green');
 
     if (count > 0) {
-        $('#basket')[0].setAttribute('visibility', 'visible');
+        $('#basket').show();
         $('#counter')[0].textContent = count;
     }
 }
 
 function removeFromBacket(name) {
     let count = getCount();
-    console.log(count);
     if (count > 0) {
         remove(name);
     }
     count = getCount();
     if (count === 0) {
         $('#counter')[0].textContent = count;
-        $('#basket')[0].setAttribute('display', 'none');
+        $('#basket').hide();
     } else {
         $('#counter')[0].textContent = count;
     }
