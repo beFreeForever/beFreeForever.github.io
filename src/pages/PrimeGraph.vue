b686f030f355736cbbd7967ebc584cbf79d6d9e7
a_kryuchenkov <a.kryuchenkov2@gmail.com>
2018-05-13 19:13:16 +0300

    fix

--- src/pages/PrimeGraph.vue
+++ src/pages/PrimeGraph.vue
@@ -1,51 +1,93 @@
 <template>
-  <b-container>
-    <section>
-      <br>
-      <h1 class="text-center">В этом месяце</h1>
-      <br>
-      <b-row>
-        <b-col sm="12" md="6" xl="4"
-          v-for="(item, i) in currentMonth" :key="i">
-          <b-card 
-              :title="item.title"
-              :img-src="image"
-              :img-alt="item.title"
-              img-top
-              tag="article"
-              class="mb-2">
-              <p class="card-text">
-                {{item.description}}
-              </p>
-              <p class="card-text">Дата выхода: {{item.date}}</p>
-              <b-button href="#">Подробнее...</b-button>
-            </b-card>
-        </b-col>
-      </b-row>
-    </section>
-     <section>
-      <br>
-      <h1 class="text-center">В следующем месяце</h1>
-      <br>
-      <b-row>
-        <b-col sm="12" md="6" xl="4"
-          v-for="(item, i) in nextMonth" :key="i">
-          <b-card 
-              :title="item.title"
-              :img-src="image"
-              :img-alt="item.title"
-              img-top
-              tag="article"
-              class="mb-2">
-              <p class="card-text">
-                {{item.description}}
-              </p>
-              <p class="card-text">Дата выхода: {{item.date}}</p>
-              <b-button href="#">Подробнее...</b-button>
-            </b-card>
-        </b-col>
-      </b-row>
-    </section>
+  <b-container fluid>
+    <b-row>
+      <b-col sm="2">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <b-list-group>
+          <b-list-group-item>Январь 2018</b-list-group-item>
+          <b-list-group-item>Февраль 2018</b-list-group-item>
+          <b-list-group-item>Март 2018</b-list-group-item>
+          <b-list-group-item>Апрель 2018</b-list-group-item>
+          <b-list-group-item active>Май 2018</b-list-group-item>
+          <b-list-group-item>Июнь 2018</b-list-group-item>
+          <b-list-group-item>Июль 2018</b-list-group-item>
+          <b-list-group-item>Август 2018</b-list-group-item>
+          <b-list-group-item>Сентябрь 2018</b-list-group-item>
+          <b-list-group-item>Октябрь 2018</b-list-group-item>
+          <b-list-group-item>Ноябрь 2018</b-list-group-item>
+          <b-list-group-item>Декабрь 2018</b-list-group-item>
+          <b-list-group-item>Январь 2019</b-list-group-item>
+          <b-list-group-item>Февраль 2019</b-list-group-item>
+          <b-list-group-item>Март 2019</b-list-group-item>
+        </b-list-group>
+      </b-col>
+      <b-col sm="8">
+        <section>
+          <br>
+          <h1 class="text-center">В этом месяце</h1>
+          <br>
+          <b-row>
+            <b-col sm="12" md="6" xl="4"
+              v-for="(item, i) in currentMonth" :key="i">
+              <b-card 
+                  :title="item.title"
+                  :img-src="image"
+                  :img-alt="item.title"
+                  img-top
+                  tag="article"
+                  class="mb-2">
+                  <p class="card-text">
+                    {{item.description}}
+                  </p>
+                  <p class="card-text">Дата выхода: {{item.date}}</p>
+                  <b-button href="#">Подробнее...</b-button>
+                </b-card>
+            </b-col>
+          </b-row>
+        </section>
+        <section>
+          <br>
+          <h1 class="text-center">В следующем месяце</h1>
+          <br>
+          <b-row>
+            <b-col sm="12" md="6" xl="4"
+              v-for="(item, i) in nextMonth" :key="i">
+              <b-card 
+                  :title="item.title"
+                  :img-src="image"
+                  :img-alt="item.title"
+                  img-top
+                  tag="article"
+                  class="mb-2">
+                  <p class="card-text">
+                    {{item.description}}
+                  </p>
+                  <p class="card-text">Дата выхода: {{item.date}}</p>
+                  <b-button href="#">Подробнее...</b-button>
+                </b-card>
+            </b-col>
+          </b-row>
+        </section>
+      </b-col>
+      <b-col sm="2">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <b-list-group>
+          <b-list-group-item>Веном - 9.9</b-list-group-item>
+          <b-list-group-item>Дедпул 2 - 9.7</b-list-group-item>
+          <b-list-group-item>Мстители 4 - 9.0</b-list-group-item>
+          <b-list-group-item>Аквамен - 8.2</b-list-group-item>
+          <b-list-group-item>Валериан 2 - 8.1</b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
