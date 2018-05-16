5322091485ad1aa4b6e48125195bef45c9f0454c
a_kryuchenkov <a.kryuchenkov2@gmail.com>
2018-05-16 17:25:11 +0300

    fix

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container>
-      <br>
+      <!-- <br>
       <h1 class="text-center">Дэдпул 2</h1>
       <br>
       <b-row>
@@ -30,7 +30,53 @@
               </b-row>
           </b-col>
       </b-row>
-      <br><br>
+      <br><br> -->
+    <br>
+    <section>    
+        <b-img 
+            src="https://s3.amazonaws.com/ffe-ugc/intlportal2/dev-temp/en-US/__5af9ceb66442e.jpg"
+            fluid/>
+        <br>
+        <br>
+    </section>
+    <section>
+        <b-row>
+            <b-col sm="12" md="6">
+                <p><b>Продолжительность: </b> 120 минут (2:00)</p>
+                <p><b>Жанр: </b> фантастика, боевик, комедия</p>
+                <p><b>Режиссер: </b> Дэвид Литч</p>
+                <p><b>Страна: </b>США</p>
+                <p><b>Оригинальное название: </b> Deadpool 2</p>
+                <p><b>Описание: </b> <br> 
+                    Во исполнение своей мечты и выброса адреналина Дэдпул снова ищет приключений, вооруженный острым ножом и нестандартным чувством юмора. Такому мощному оружию не могут противостоять ни ниндзя, ни якудза, ни даже свора буйных сексуально озабоченных собак. В пылу сражений, где кровь смешивается с пошлыми шутками, даже такой циник как Дэдпул начинает задумываться о семейных ценностях и дружбе. Впрочем, эта шальная мысль вряд ли сможет изменить его натуру.
+                </p>
+                <p><b>Возрастное ограничение: </b> 18+</p>
+            </b-col>
+            <b-col sm="12" md="6">
+                <b-row >
+                    <b-col>
+                        <h2 class="text-center">Рейтинги</h2>
+                        <br>
+                        <b-row>
+                            <b-col>
+                                <p><b>Кинопоиск: </b> 8.7</p>
+                            </b-col>
+                            <b-col>
+                                <p><b>Кинопоиск: </b> 8.7</p>
+                            </b-col>
+                            <b-col>
+                                <p><b>Кинопоиск: </b> 8.7</p>
+                            </b-col>
+                        </b-row>
+                    </b-col>
+                    <b-col sm="12">
+                        <iframe width="100%" src="https://www.youtube.com/embed/xFvOZAepx4I" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+    </section>
+
   </b-container>
 </template>
 
